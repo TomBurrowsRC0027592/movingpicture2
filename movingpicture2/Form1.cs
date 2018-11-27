081d0c99e0112928a9c1ceb1025ef953c0c5e4c6
Tom Burrows <rc0027592@student.riversidecollege.ac.uk>
2018-11-27 09:34:10 +0000

    was working for stopping on the page boarder

--- movingpicture2/Form1.cs
+++ movingpicture2/Form1.cs
@@ -25,13 +25,17 @@ namespace movingpicture2
         private void btnbutton2_Click(object sender, EventArgs e)
         {
             if (pictureBox1.Location.X > 0)
-            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+            }
         }
-
+                                  
         private void btnbutton3_Click(object sender, EventArgs e)
         {
             if (pictureBox1.Location.X + 5 < ClientRectangle.Width)
-            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+            }
         }
 
         private void button1_Click_1(object sender, EventArgs e)
