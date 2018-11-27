f09ad88e274c095c7ad62a5c5f81e2a34c74ceac
Tom Burrows <rc0027592@student.riversidecollege.ac.uk>
2018-11-27 10:47:49 +0000

    up to date to task 5

--- movingpicture2/Form1.Designer.cs
+++ movingpicture2/Form1.Designer.cs
@@ -36,6 +36,7 @@ namespace movingpicture2
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -89,11 +90,22 @@ namespace movingpicture2
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(712, 139);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "to the right";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -113,6 +125,7 @@ namespace movingpicture2
         private Button button2;
         private Button button3;
         private Button button4;
+        private Button button5;
     }
 }
 
--- movingpicture2/Form1.cs
+++ movingpicture2/Form1.cs
@@ -24,28 +24,41 @@ namespace movingpicture2
 
         private async void button2_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X - 30, pictureBox1.Location.Y);
-            await Task.Delay(500);
-            pictureBox1.Location = new Point(pictureBox1.Location.X + 30, pictureBox1.Location.Y);
-            await Task.Delay(500);
+            for (int i = 0; i < 10; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 50, pictureBox1.Location.Y);
+                await Task.Delay(250);
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 50, pictureBox1.Location.Y);
+                await Task.Delay(250);
+            }
+            
+            
+            
         }
                                   
         private async void button3_Click(object sender, EventArgs e)
         {
-            while (pictureBox1.Location.X + 5 < ClientRectangle.Width - pictureBox1.Width)
+            for (int i = 0; i < 10; i++)
             {
-                pictureBox1.Location = new Point(pictureBox1.Location.X + 30, pictureBox1.Location.Y);
-                await Task.Delay(100);
-                pictureBox1.Location = new Point(pictureBox1.Location.X - 30, pictureBox1.Location.Y);
-                await Task.Delay(500);
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 50, pictureBox1.Location.Y);
+                await Task.Delay(250);
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 50, pictureBox1.Location.Y);
+                await Task.Delay(250);
             }
-            
-
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
         }
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            while (pictureBox1.Location.X + 5 < ClientRectangle.Width - pictureBox1.Width)
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+            await Task.Delay(100);
+        }
+
+        
+        }
     }
-}
